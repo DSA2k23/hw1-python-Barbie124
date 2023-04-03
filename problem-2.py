ba2e59c50aefe0bbcf975a2332d54590174fe9e2
Barbie124 <129590240+Barbie124@users.noreply.github.com>
2023-04-04 00:15:28 +0400

    Update problem-2.py

---
+++ problem-2.py
@@ -0,0 +1,9 @@
+def base(n):
+    if n == 0:
+        return '0'
+    num_list = []
+    while n:
+        n, r = divmod(n, 3)
+        num_list.append(str(r))
+    return ''.join(reversed(num_list))
+base()
